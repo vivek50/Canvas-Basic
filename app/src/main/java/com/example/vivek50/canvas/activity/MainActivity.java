e7ae824087b2e4e0c851573439a7f2e6cfafab18
vivek50 <vivekmodi50>
2017-08-01 23:41:22 +0530

    Simple Basics...

---
+++ app/src/main/java/com/example/vivek50/canvas/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.vivek50.canvas.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.vivek50.canvas.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
