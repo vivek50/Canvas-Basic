9048be578cd82b2366c49e75fbda685886c3aa7f
vivek50 <vivekmodi50>
2017-08-02 00:07:22 +0530

    Simple Basics...

--- app/src/main/java/com/example/vivek50/canvas/activity/MainActivity.java
+++ app/src/main/java/com/example/vivek50/canvas/activity/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.vivek50.canvas.activity;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.example.vivek50.canvas.R;
 
@@ -11,5 +20,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyView myView = new MyView(this);
+        addContentView(myView,new ViewGroup.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT));
+
+    }
+
+    private class MyView extends View {
+
+        public MyView (Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+
+            Paint paint = new Paint();
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.WHITE);
+            canvas.drawPaint(paint);
+
+            paint.setAntiAlias(true);
+            paint.setColor(Color.BLUE);
+            canvas.drawCircle(40, 40, 15, paint);
+        }
     }
 }
+
+
